974b756251af4f2e72f5b58a2d7214b2dbcc907b
frapastique <frapastique@outlook.com>
2023-04-12 14:07:03 +0200

    code AppRepository.kt

--- app/src/main/java/com/back/frapuse/data/AppRepository.kt
+++ app/src/main/java/com/back/frapuse/data/AppRepository.kt
@@ -1,4 +1,24 @@
 package com.back.frapuse.data
 
-class AppRepository {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.back.frapuse.data.datamodels.TextToImage
+import com.back.frapuse.data.remote.TextToImageAPI
+
+const val TAG = "AppRepository"
+
+class AppRepository(private val api: TextToImageAPI) {
+
+    private var _image = MutableLiveData<TextToImage>()
+    val image: LiveData<TextToImage>
+        get() = _image
+
+    suspend fun getPrompt(prompt: String, steps: Int, width: Int, height: Int) {
+        try {
+            _image.value = api.retrofitService.getPrompt(prompt, steps, width, height)
+        } catch (e: Exception) {
+            Log.e(TAG, "Error loading Data from API: $e")
+        }
+    }
 }

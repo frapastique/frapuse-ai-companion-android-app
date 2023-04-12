6a3f40d1f02d137df1582b7c57402521dc64daa2
frapastique <frapastique@outlook.com>
2023-04-12 14:22:46 +0200

    code base SharedViewModel.kt

--- app/src/main/java/com/back/frapuse/SharedViewModel.kt
+++ app/src/main/java/com/back/frapuse/SharedViewModel.kt
@@ -1,6 +1,32 @@
 package com.back.frapuse
 
+import android.media.Image
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.back.frapuse.data.AppRepository
+import com.back.frapuse.data.datamodels.TextToImage
+import com.back.frapuse.data.remote.TextToImageAPI
+import kotlinx.coroutines.launch
 
 class SharedViewModel : ViewModel() {
+
+    private val repository = AppRepository(TextToImageAPI)
+
+    val imageBase64 = repository.image
+
+    private var _image = MutableLiveData<Image>()
+    val image: LiveData<Image>
+        get() = _image
+
+    fun loadPrompt(prompt: String) {
+        viewModelScope.launch {
+            repository.getPrompt(prompt, 5, 256, 256)
+        }
+    }
+
+    fun decodeImage(imageBase64: TextToImage) {
+        TODO("Convert Base64 image to image")
+    }
 }

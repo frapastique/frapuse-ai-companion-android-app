785ec1f1fca433e27447b38538b635a178370995
frapastique <frapastique@outlook.com>
2023-04-24 13:23:04 +0200

    add files for RecyclerView

--- app/src/main/java/com/back/frapuse/ui/imagegen/TextToImageFragment.kt
+++ app/src/main/java/com/back/frapuse/ui/imagegen/TextToImageFragment.kt
@@ -1,8 +1,7 @@
-package com.back.frapuse.ui
+package com.back.frapuse.ui.imagegen
 
 import android.content.res.ColorStateList
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View

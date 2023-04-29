a1f73fa5aa01d77ee641ea005c917e734f3ae0e4
frapastique <frapastique@outlook.com>
2023-04-29 02:27:18 +0200

    add curl cmd for reference

--- app/src/main/java/com/back/frapuse/data/datamodels/textgen/TextGenTokenCountResponse.kt
+++ app/src/main/java/com/back/frapuse/data/datamodels/textgen/TextGenTokenCountResponse.kt
@@ -3,3 +3,12 @@ package com.back.frapuse.data.datamodels.textgen
 data class TextGenTokenCountResponse(
     val results: List<TextGenTokenCountBody>
 )
+
+/*
+
+curl -X POST \
+-H "Content-Type: application/json" \
+-d "{\"prompt\": \"In order to make homemade bread, follow these steps:\n1)\"}" \
+http://192.168.178.20:7863/api/v1/token-count
+
+*/

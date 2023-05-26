174ae4b62d9398348ab25b7cb9704434268773b1
frapastique <frapastique@outlook.com>
2023-05-26 12:09:00 +0200

    add some files

---
+++ app/src/main/java/com/back/frapuse/data/textgen/models/TextGenHaystackFileUpload.kt
@@ -0,0 +1,20 @@
+package com.back.frapuse.data.textgen.models
+
+import com.squareup.moshi.JsonClass
+import java.io.File
+
+@JsonClass(generateAdapter = true)
+data class TextGenHaystackFileUpload(
+    val files: File,
+    val meta: String?,
+    val additional_params: String?,
+    val remove_numeric_tables: Boolean,
+    val valid_languages: List<String>,
+    val clean_whitespace: Boolean,
+    val clean_empty_lines: Boolean,
+    val clean_header_footer: Boolean,
+    val split_by: String,
+    val split_length: Int,
+    val split_overlap: Int,
+    val split_respect_sentence_boundary: Boolean
+)
